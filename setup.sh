8f119ae214809297def1ff9e8af02f0264fa80bc
Drew Monroe <d.monroe@snet.net>
2016-10-20 11:20:38 -0400

    Modify setup.sh to install pathogen

--- setup.sh
+++ setup.sh
@@ -4,13 +4,21 @@
 rm ~/.bashrc
 ln -s $PWD/.bashrc ~/.bashrc
 rm ~/.vimrc
+
+# Get vim all set up
 ln -s $PWD/.vimrc ~/.vimrc
 rm ~/.vim
 ln -s $PWD/.vim ~/.vim
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Set up tmux
 rm ~/.tmux.conf
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 rm ~/.tmux_status
 ln -s $PWD/.tmux_status ~/.tmux_status
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
+
 echo "prefix + I to get tmux plugins"
